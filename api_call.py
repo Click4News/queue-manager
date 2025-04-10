6e42e317a8eca46475fdf3fdd3ba4396064220fe
Varun Hoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2025-04-09 18:14:04 -0600

    added limit to num of articles requested

--- api_call.py
+++ api_call.py
@@ -8,17 +8,18 @@ API_KEY = os.environ.get('API_KEY')
 BASE_URL = 'https://eventregistry.org/api/v1/article/getArticles'
 
 
-def make_api_call(which_city: str):
+def make_api_call(which_city: str, num_articles: int = 100):
     query = {
-    "$query": {"locationUri": f"http://en.wikipedia.org/wiki/{which_city.replace(' ', '_')}"},
-    "$filter": {"forceMaxDataTimeWindow": "31"}
+        "$query": {"locationUri": f"http://en.wikipedia.org/wiki/{which_city.replace(' ', '_')}"},
+        "$filter": {"forceMaxDataTimeWindow": "31"}
     }
 
     params = {
         'query': json.dumps(query),
         'resultType': 'articles',
         'articlesSortBy': 'date',
-        'apiKey': API_KEY
+        'apiKey': API_KEY,
+        'articlesCount': num_articles
     }
     
     response = requests.get(BASE_URL, params=params)
@@ -26,5 +27,6 @@ def make_api_call(which_city: str):
     return response.json()
 
 if __name__ == "__main__":
-    news = make_api_call('Denver')
-    print(news)
+    news = make_api_call('Denver', 9)
+    print(json.dumps(news, indent=4))
+    print(len(news['articles']['results']))

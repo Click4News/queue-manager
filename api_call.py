b95003c0b835de7b7f1abac4ec66a3a5ee2e7f00
Varun Hoskere <varunshankarhoskere@Varuns-MacBook-Pro.local>
2025-04-17 09:23:22 -0600

    force include state in API call

--- api_call.py
+++ api_call.py
@@ -2,13 +2,16 @@ import requests
 import json
 # from constants import API_KEY
 import os
-
-API_KEY = os.environ.get('API_KEY')
+from cities import CITIES_DICT
+# API_KEY = os.environ.get('API_KEY')
 
 BASE_URL = 'https://eventregistry.org/api/v1/article/getArticles'
-
+API_KEY = 'f9a1b16e-6dd1-4f71-b009-f856160b2cf6'
 
 def make_api_call(which_city: str, num_articles: int = 100):
+    state = CITIES_DICT[which_city]
+    which_city += f', {state}'
+    print(which_city)
     query = {
         "$query": {"locationUri": f"http://en.wikipedia.org/wiki/{which_city.replace(' ', '_')}"},
         "$filter": {"forceMaxDataTimeWindow": "31"}
@@ -21,12 +24,15 @@ def make_api_call(which_city: str, num_articles: int = 100):
         'apiKey': API_KEY,
         'articlesCount': num_articles
     }
+
+    print(params)
     
     response = requests.get(BASE_URL, params=params)
     response.raise_for_status()
     return response.json()
 
 if __name__ == "__main__":
-    news = make_api_call('Denver', 9)
+    news = make_api_call('Huntsville', 1)
     print(json.dumps(news, indent=4))
+    print(json.dumps(news['articles']['results']))
     print(len(news['articles']['results']))
